9d40ffd931a4286b916f66ce095157e4b69a9a6e
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2023-12-13 20:38:03 +0300

    alembic migrations

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,2 @@
+from src.models.kanban_board import KanbanBoard
 from src.models.user_account import UserAccount
--- src/models/user_account.py
+++ src/models/user_account.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
+
+if TYPE_CHECKING:
+    from src.models.kanban_board import KanbanBoard
 
 
 class UserAccount(Base):
@@ -9,6 +14,7 @@ class UserAccount(Base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    kanban_board: Mapped[list["KanbanBoard"]] = relationship(back_populates="user_account")
 
     def __repr__(self) -> str:
         return f"{self.email}"
